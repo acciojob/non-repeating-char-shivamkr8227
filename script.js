977493e5e278a97c2708d75509accd9367aea3b9
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-18 00:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function firstnrc(str)
+	{
+		for(let i=0; i<atr.length ; i++)
+		{
+			if(str,indexof(str[i])===str.lastindexof(str[i]))
+			{
+				return str[i];
+			}
+		}
+	}
+ 
+let inputStr=prompt("Enter a string");
+let result=firstnrc(inputStr);
+alert(result);
